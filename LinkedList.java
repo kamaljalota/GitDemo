41417da3210df3ab9fdfb2f57e61aa0bf6c4df76
Harshit Dwivedi <harshit@pitech.app>
2019-04-01 11:33:56 +0530

    added method to add a node at the end

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,16 @@
-class LinedList{
+class LinkedList{
 
 	//TODO : Write code for linkedlist here
 	
 	public void reversal(){
 	
 		//TODO : Write the code for reversal here
+	}
+
+	public void insert(LinkedList ll){
+
+		//TODO : Write code for inserting an item to the end of the linked list	
+
 	}
 
 }
