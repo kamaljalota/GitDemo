d40369b6f943206a9295a47dbeed9d19837f5cda
Harshit Dwivedi <harshit@pitech.app>
2019-04-01 11:41:09 +0530

    test:q iRevert "last class ka badla"
    This reverts commit f25bc4e02568335d12ca4e3dd826c7aafb7811f7.

---
+++ LinkedList.java
@@ -0,0 +1,17 @@
+class LinkedList{
+
+	//TODO : Write code for linkedlist here
+	
+	public void reversal(){
+	
+		//TODO : Write the code for reversal here
+	}
+
+	public void insert(LinkedList ll){
+		
+		//Fix the bug
+		//TODO : Write code for inserting an item to the end of the linked list	
+
+	}
+
+}
